630ab39192888a351933bd5cc110fb6a8540a690
kraj2503 <73212876+kraj2503@users.noreply.github.com>
2026-02-24 23:36:16 +0530

    Apparantly we cannot call a predefined tool and a function together in a agent call

--- Jarvis/multi_agent/healthcare_agent.py
+++ Jarvis/multi_agent/healthcare_agent.py
@@ -1,16 +1,28 @@
-from google.adk.agents  import LlmAgent
+from google.adk.agents  import Agent
 from google.adk.tools import google_search
 from Jarvis.tools.healthcare_tool import get_healthcare_similarity
 from Jarvis.multi_agent.instructions import get_instructions
 from google.adk.models import Gemini
 
-healthcare_agent = LlmAgent(
+
+
+healthcare_agent_google = Agent(
     name="healthcare_agent",
     model=Gemini(model="models/gemini-3-flash-preview"),
 
 
     static_instruction=get_instructions("healthcare_agent"),
     
+    tools=[google_search]   
+)
+
+healthcare_agent = Agent(
+    name="healthcare_agent",
+    model=Gemini(model="models/gemini-3-flash-preview"),
+
+
+    static_instruction=get_instructions("healthcare_agent"),
+    sub_agents=[healthcare_agent_google],
     tools=[get_healthcare_similarity]   
 )
 
