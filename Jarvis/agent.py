d451d00eb886ba1309420d5cc64d0607fc00cf78
kraj2503 <73212876+kraj2503@users.noreply.github.com>
2026-02-25 22:20:39 +0530

    a2a

--- Jarvis/agent.py
+++ Jarvis/agent.py
@@ -6,10 +6,10 @@ from google.adk.sessions import database_session_service
 from google.adk.sessions import InMemorySessionService
 from Jarvis.tools.memory_tool import read_memory, write_memory
 from Jarvis.instructions import get_instructions
-
+from google.genai import types
 # from google.adk.agents.remote_a2a_agent import PREV_AGENT_CARD_WELL_KNOWN_PATH,
 from google.adk.agents.remote_a2a_agent import RemoteA2aAgent
-
+from google.adk.agents.remote_a2a_agent import AGENT_CARD_WELL_KNOWN_PATH
 # from google.adk.runners import Runner
 
 
@@ -22,14 +22,14 @@ MODEL = "gemini-3-flash-preview"
 # InMemorySessionService stores conversations in RAM (temporary)
 session_service = InMemorySessionService()    
 
-
-
+REMOTE_URL="http://localhost:8001"
+AGENT_CARD=f"{REMOTE_URL}{AGENT_CARD_WELL_KNOWN_PATH}"
+print(f"\n\n\n AGENT CARD URL--  {AGENT_CARD} \n\n\n\n")
 home_automation_remote_agent = RemoteA2aAgent(
     name="Home_automation_agent",
-    description="Agent handles all the home automation tasks for the user",
-    agent_card=(
-        f"http://localhost:8001/.well-known/agent-card.json"
-    ),
+    description="Agent handles all the home automation tasks...",
+    httpx_client=REMOTE_URL,
+    agent_card=AGENT_CARD
 )
 
 
@@ -45,9 +45,12 @@ root_agent = LlmAgent(
     # after_model_callback=[read_memory],
     sub_agents=[healthcare_agent,financial_agent,home_automation_remote_agent],
     #session_service=session_service,
-    tools =[read_memory,
-            write_memory
-            
-            ]    
-
+    generate_content_config=types.GenerateContentConfig(
+        safety_settings=[
+            types.SafetySetting(  # avoid false alarm about rolling dice.
+                category=types.HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
+                threshold=types.HarmBlockThreshold.OFF,
+            ),
+        ]  
+    )
 )
--- homer/agent.py
+++ homer/agent.py
@@ -2,10 +2,13 @@ from google.adk.agents.llm_agent import Agent
 from google.adk.a2a.utils.agent_to_a2a import to_a2a
 from homer.multi_agent import room_automation_agent
 from dotenv import load_dotenv
+from google.adk.models import Gemini
+
 load_dotenv()
 
 root_agent = Agent(
-    model='gemini-2.5-flash',
+    model=Gemini(model="models/gemini-3-pro-preview"),
+
     name='root_agent',
     description='Home automation Agent',
     instruction='You are an home automation agent your task is to do the home automation as asked and reply back to user when task is completed',
@@ -13,4 +16,4 @@ root_agent = Agent(
 )
 
 
-app = to_a2a(root_agent)
+app = to_a2a(root_agent, port=8001,protocol="http")
