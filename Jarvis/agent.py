34de4289bf31d61325b90e0f6629b5e238f71b19
kraj2503 <73212876+kraj2503@users.noreply.github.com>
2026-02-26 01:27:05 +0530

    Agent minor changes

--- Jarvis/agent.py
+++ Jarvis/agent.py
@@ -9,26 +9,32 @@ from Jarvis.instructions import get_instructions
 from google.genai import types
 # from google.adk.agents.remote_a2a_agent import PREV_AGENT_CARD_WELL_KNOWN_PATH,
 from google.adk.agents.remote_a2a_agent import RemoteA2aAgent
+from google.adk.runners import Runner
 from google.adk.agents.remote_a2a_agent import AGENT_CARD_WELL_KNOWN_PATH
 # from google.adk.runners import Runner
+import httpx
 
 
 APP_NAME = "Jarvis"
-USER_ID = "mem_user"
+# USER_ID = "mem_user"
 MODEL = "gemini-3-flash-preview" 
 
 
 
 # InMemorySessionService stores conversations in RAM (temporary)
-session_service = InMemorySessionService()    
+session_service = InMemorySessionService()  #Will migrate to redis later    
 
 REMOTE_URL="http://localhost:8001"
 AGENT_CARD=f"{REMOTE_URL}{AGENT_CARD_WELL_KNOWN_PATH}"
+
+
 print(f"\n\n\n AGENT CARD URL--  {AGENT_CARD} \n\n\n\n")
+
+
 home_automation_remote_agent = RemoteA2aAgent(
     name="Home_automation_agent",
     description="Agent handles all the home automation tasks...",
-    httpx_client=REMOTE_URL,
+    httpx_client=httpx.AsyncClient(base_url=REMOTE_URL),
     agent_card=AGENT_CARD
 )
 
@@ -45,12 +51,14 @@ root_agent = LlmAgent(
     # after_model_callback=[read_memory],
     sub_agents=[healthcare_agent,financial_agent,home_automation_remote_agent],
     #session_service=session_service,
-    generate_content_config=types.GenerateContentConfig(
-        safety_settings=[
-            types.SafetySetting(  # avoid false alarm about rolling dice.
-                category=types.HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
-                threshold=types.HarmBlockThreshold.OFF,
-            ),
-        ]  
-    )
+    # generate_content_config=types.GenerateContentConfig(
+    #     safety_settings=[
+    #         types.SafetySetting(  # avoid false alarm about rolling dice.
+    #             category=types.HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
+    #             threshold=types.HarmBlockThreshold.OFF,
+    #         ),
+    #     ]  
+    # )
 )
+
+runner = Runner(app_name="Jarvis", agent=root_agent,session_service=session_service)
--- homer/agent.py
+++ homer/agent.py
@@ -9,7 +9,7 @@ load_dotenv()
 root_agent = Agent(
     model=Gemini(model="models/gemini-3-pro-preview"),
 
-    name='root_agent',
+    name='home_automation_agent',
     description='Home automation Agent',
     instruction='You are an home automation agent your task is to do the home automation as asked and reply back to user when task is completed',
     sub_agents=[room_automation_agent]
